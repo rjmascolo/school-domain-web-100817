f22c45a92e93934f93269182c00901cd9d5e3712
github username <github email address>
2017-10-11 11:44:13 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -12,18 +12,18 @@ class School
 
   def add_student(name, grade)
     if roster[grade]
-      @roster[grade] << name
+      self.roster[grade] << name
     else
-      @roster[grade] = [name]
+      self.roster[grade] = [name]
     end
   end
 
   def grade(search_grade)
-    @roster[search_grade]
+    self.roster[search_grade]
   end
 
   def sort
-    @roster.map { |k,v| [k,v.sort] }.to_h
+    self.roster.map { |k,v| [k,v.sort] }.to_h
   end
 
 end
